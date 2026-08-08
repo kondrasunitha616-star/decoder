16067ae1f3d9b8add2455d307f3ec9e230fb411f
kondrasunitha616-star <kondrasunitha616@gmail.com>
2026-08-08 22:41:35 +0530

    Add files via upload

---
+++ decoder/Decoder design — decoder_2to4.v
@@ -0,0 +1,23 @@
+Decoder design — decoder_2to4.v
+module decoder_2to4 (
+    input  wire [1:0] A,
+    input  wire       EN,
+    output reg  [3:0] Y
+);
+
+always @(*) begin
+    if (EN) begin
+        case (A)
+            2'b00: Y = 4'b0001;
+            2'b01: Y = 4'b0010;
+            2'b10: Y = 4'b0100;
+            2'b11: Y = 4'b1000;
+            default: Y = 4'b0000;
+        endcase
+    end
+    else begin
+        Y = 4'b0000;
+    end
+end
+
+endmodule
